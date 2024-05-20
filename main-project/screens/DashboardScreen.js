9d64e4a5794091f7f316086892faad5e07caacb3
SMK-008 <sammy.kitonga@strathmore.edu>
2024-05-20 14:33:03 +0300

    added logout button in DashboardScreen

--- main-project/screens/DashboardScreen.js
+++ main-project/screens/DashboardScreen.js
@@ -1,11 +1,25 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+// import { useNavigation } from '@react-navigation/native';
+
+
 
 const DashboardScreen = () => {
+  const navigation=useNavigation()
+
+  const logOut=()=>{
+    navigation.navigate('Login')
+   }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Dashboard</Text>
-      <Text>Welcome to the dashboard!</Text>
+      {/* Below will display the user's name in the current sesstion */}
+      <Text style={styles.welcomeTxt}>Welcome user</Text>
+      <TouchableOpacity onPress={logOut} style={styles.logout}>
+        <Text style={styles.logoutTxt}>Logout</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -18,10 +32,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#f5f5f5',
   },
   title: {
-    fontSize: 24,
+    fontSize: 30,
     fontWeight: 'bold',
     marginBottom: 20,
   },
+  welcomeTxt:{
+    fontSize: 20
+  },
+  logout:{
+    width: 200,
+    padding: 10,
+    backgroundColor: 'grey',
+    borderRadius: 15,
+    alignItems: 'center',
+    marginTop: 10,
+    position:'absolute',
+    bottom:10,
+  },
+  logoutTxt:{
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginRight: 10,
+    color:'lightblue'
+  }
 });
 
 export default DashboardScreen;
