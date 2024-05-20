59e92f11f5008c758961ef5aeda6b98d228c4b8b
ShalomObongo <145100315+ShalomObongo@users.noreply.github.com>
2024-05-20 18:52:34 +0300

    UI changes implementation

--- main-project/screens/HomeScreen.js
+++ main-project/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { auth } from '../firebase'
 
@@ -17,10 +17,13 @@ const Homescreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to Student-Sphere</Text>
-      <Button
-        title="Login" onPress={handleLogin} />
-      <Button
-        title="Register" onPress={handleRegister} />
+      <Text style={styles.homeTxt}>What would you like to do?</Text>
+      <TouchableOpacity style={styles.loginBtn} onPress={handleLogin}>
+        <Text style={styles.homeTxt}>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.registerBtn} onPress={handleRegister}>
+        <Text style={styles.homeTxt}>Register</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -33,10 +36,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#f5f5f5',
   },
   title: {
-    fontSize: 24,
+    fontSize: 25,
     fontWeight: 'bold',
     marginBottom: 20,
   },
+  loginBtn: {
+    width: 200,
+    padding: 10,
+    backgroundColor: 'lightgrey',
+    borderRadius: 15,
+    alignItems: 'center',
+    marginTop: 10
+  },
+  registerBtn: {
+    width: 200,
+    padding: 10,
+    backgroundColor: '#7fffd4',
+    borderRadius: 15,
+    alignItems: 'center',
+    marginTop: 10
+  },
+  homeTxt: {
+    fontSize: 20
+  }
 });
 
 export default Homescreen;
