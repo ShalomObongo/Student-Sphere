9d38062a8a6481c232d62ca83f131128e8866609
SMK-008 <sammy.kitonga@strathmore.edu>
2024-05-20 18:49:04 +0300

    added firestore database initialization

--- main-project/firebase.js
+++ main-project/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -22,4 +23,6 @@ const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage)
 });
 
-export { auth };
+const db = getFirestore(app);
+
+export { db,auth };
