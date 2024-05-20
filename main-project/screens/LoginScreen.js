d321493030e0d108b2a7f3adca960425af723f9d
SMK-008 <sammy.kitonga@strathmore.edu>
2024-05-20 11:17:58 +0300

    improved Loginscreen appearance

--- main-project/screens/LoginScreen.js
+++ main-project/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { KeyboardAvoidingView, View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import { KeyboardAvoidingView, View, Text, TextInput, Button, StyleSheet, Alert, TouchableOpacity } from 'react-native';
 import { auth } from '../firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useNavigation } from '@react-navigation/native';
@@ -17,29 +17,38 @@ const LoginScreen = () => {
         Alert.alert('Login Successful', `Welcome, ${user.email}`);
         navigation.navigate('Dashboard');
       })
-      .catch((error) => Alert.alert('Login Failed', error.message));
+      .catch((error) => Alert.alert('Login Failed.Email and password does not exist', error.message));
   };
 
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.container}>
         <Text style={styles.title}>Login</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          autoCapitalize="none"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          secureTextEntry
-          value={password}
-          onChangeText={setPassword}
-        />
-        <Button title="Login" onPress={handleLogin} />
+
+        <View style={styles.inputBox}>
+          <Text style={styles.text}>Email:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter valid email"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+        </View>
+        <View style={styles.inputBox}>
+          <Text style={styles.text}>Password:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter password"
+            secureTextEntry
+            value={password}
+            onChangeText={setPassword}
+          />
+        </View>
+        <TouchableOpacity style={styles.loginBtn} title="Login" onPress={handleLogin} >
+          <Text style={styles.loginbtnText}>Login</Text>
+        </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
   );
@@ -52,20 +61,48 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#f5f5f5',
   },
+
   title: {
-    fontSize: 24,
+    fontSize: 40,
     fontWeight: 'bold',
     marginBottom: 20,
   },
+
   input: {
-    width: '80%',
-    height: 40,
+    flex:1,
+    width: '100px',
+    height: 60,
+    fontSize:18,
     borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 5,
     marginBottom: 10,
-    padding: 10,
+    padding: 20,
+    
   },
+  inputBox:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    width: '80%',
+  },
+  loginBtn:{
+    width: 200,
+    padding: 10,
+    backgroundColor: 'blue',
+    borderRadius: 15,
+    alignItems: 'center',
+    marginTop: 10
+  },
+  loginbtnText: {
+    color: 'white',
+    fontSize: 20,
+  },
+  text:{
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginRight: 10,
+  }
 });
 
 export default LoginScreen;
